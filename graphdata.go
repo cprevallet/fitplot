147b7de76f93fbcec6cbd1f1ecae5e257004f92f
Craig Prevallet <penguin@Cowabunga.PrevalletNet>
2016-06-07 17:14:10 -0500

    Refactor graphdata routines again.  More flexible but slower.  :(

--- graphdata.go
+++ graphdata.go
@@ -16,6 +16,16 @@ var paceToMetric float64 = 16.666667 // sec/meter -> min/km
 
 var toEnglish bool = true
 
+// Do unit conversion for slices of type valtype
+func convertUnits(vals []float64, valtype string, toEnglish bool) (cvtVals []float64) {
+        for _, val := range vals {
+	  converted := unitCvt(val, valtype, toEnglish)
+	  cvtVals = append(cvtVals, converted)
+	}
+	return cvtVals
+}
+
+// Convert a single value (e.g. from a slice)
 func unitCvt(val float64, valtype string, toEnglish bool) (cvtVal float64) {
 	switch {
         case valtype == "distance":
@@ -42,51 +52,62 @@ func unitCvt(val float64, valtype string, toEnglish bool) (cvtVal float64) {
   return cvtVal
 }
 
-func getDvsA(runRecs []fit.Record, toEnglish bool) (data [][]float64) {
-	var x float64
-	var y float64
+//Slice up a structure.
+func unpackRecs( runRecs []fit.Record) (distance []float64, altitude []float64, cadence []float64, speed []float64, lat []float64, lng []float64) {
         for _, record := range runRecs {
-	    x = unitCvt(record.Distance, "distance", toEnglish)
-	    y = unitCvt(record.Altitude, "altitude", toEnglish)
-	    coordpair := []float64{x,y}
-            data = append(data, coordpair)
-        }
-        return
+	  distance = append(distance, record.Distance)
+	  altitude = append(altitude, record.Altitude)
+	  cadence = append(cadence, float64(record.Cadence))
+	  speed = append(speed, record.Speed)
+	  lat = append(lat, record.Position_lat)
+	  lng = append(lng, record.Position_long)
+	}
+	return	 
+  }
+
+//Take two slices and pair the individual elements into x-y points.
+func createPlotCoordinates(xSlice []float64, ySlice []float64)(data [][]float64) {
+  for i, _ := range xSlice {
+    coordpair := []float64{xSlice[i], ySlice[i]}
+    data = append(data, coordpair)
+  }
+  return
 }
 
-func getDvsC(runRecs []fit.Record, toEnglish bool) (data [][]float64) {
-	var x float64
-	var y float64
-        for _, record := range runRecs {
-	    x = unitCvt(record.Distance, "distance", toEnglish)
-	    y = unitCvt(float64(record.Cadence), "cadence", toEnglish)
-	    coordpair := []float64{x,y}
-            data = append(data, coordpair)
-        }
-        return
+//Convert two arrays into a map used by Google maps.
+func getMapCoordinates(latSlice []float64, lngSlice []float64) (data []map[string]float64) {
+  for i, _ := range latSlice {
+    mapPos := map[string]float64{"lat": latSlice[i], "lng": lngSlice[i]}
+    data = append(data, mapPos)
+  }
+  return
 }
 
-func getDvsP(runRecs []fit.Record, toEnglish bool) (data [][]float64) {
-	var x float64
-	var y float64
-        for _, record := range runRecs {
-	    x = unitCvt(record.Distance, "distance", toEnglish)
-	    if record.Speed > 0.0 {
-	      y = unitCvt(1.0/record.Speed, "pace", toEnglish)
-	    } else {
-	      if toEnglish {y=20.0} else {y=12.0}
-	    }
-	    coordpair := []float64{x,y}
-            data = append(data, coordpair)
-        }
-        return
-}
+//Main entry point
+func processFitRecord(runRecs []fit.Record, toEnglish bool)( mapData []map[string]float64,Y0Pairs [][]float64, Y1Pairs [][]float64, Y2Pairs [][]float64 ) {
 
-func getlatlong(runRecs []fit.Record) (data []map[string]float64) {
-        for _, record := range runRecs {
-	    mapPos := map[string]float64{"lat": record.Position_lat, "lng": record.Position_long}
-            data = append(data, mapPos)
-        }
-        return
-}
-
+    // Get slices from the runRecs structure.
+    distance, altitude, cadence, speed, lat, lng := unpackRecs(runRecs)
+    // Speed -> pace
+    var pace []float64
+    for i, _ := range(speed) {
+      if speed[i] > 0.1 {
+	pace = append(pace, 1.0/speed[i])
+      } else {
+	pace = append(pace, 12.0 )  //min/km
+      }
+    }
+    // Convert the units for the slices that have them.
+    dispDistance := convertUnits(distance, "distance", toEnglish)
+    dispPace := convertUnits(pace, "pace", toEnglish)
+    dispAltitude := convertUnits(altitude, "altitude", toEnglish)
+    dispCadence := convertUnits(cadence, "cadence", toEnglish)
+    
+    //Return the values used in the user interface.
+    Y0Pairs = createPlotCoordinates(dispDistance, dispPace)
+    Y1Pairs = createPlotCoordinates(dispDistance, dispAltitude)
+    Y2Pairs = createPlotCoordinates(dispDistance, dispCadence)
+    mapData = getMapCoordinates(lat, lng)
+    
+    return
+  }
