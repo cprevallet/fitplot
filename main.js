c09987799b7dd4c6c1578ea2fcd7a4954054fe6b
Craig Prevallet <penguintx@hotmail.com>
2019-02-06 20:44:24 -0600

    Call the server using different paths depending on the operating system.

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 // initialize the server
+
 var exec = require('child_process').exec;
-exec('fitplot &', function callback(error, stdout, stderr){
-    // result
-});
 
-
+// Windows, fitplot with be in the path 
+if (navigator.platform.substring(0,3) == "Win") {
+	exec('fitplot &', function callback(error, stdout, stderr){
+    	if (error) {
+        	console.log("child processes failed with error code: " +
+            	error.code);
+    	}
+    	console.log(stdout);
+	});
+  };
+  
+// Linux, need to use a relative path
+if (navigator.platform.substring(0,3) == "Lin") {
+	exec('./fitplot &', function callback(error, stdout, stderr){
+    	if (error) {
+        	console.log("child processes failed with error code: " +
+            	error.code);
+    	}
+    	console.log(stdout);
+	});
+  };  
+  
 // and ...
 nw.Window.open('localhost:8080/', {width:1024, height:768}, function(win) {});
