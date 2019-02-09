ba76c826d7e78727fd0535ef0d5dd74eeea5a5bc
Craig Prevallet <penguintx@hotmail.com>
2019-02-08 18:02:50 -0600

    Add developer scripts.

---
+++ debugme.sh
@@ -0,0 +1,3 @@
+# assumes fitplot executable has bin copied from ~/go/bin to here
+cp ~/go/bin/fitplot .
+env STATIC_FILES=/home/craig/go/src/github.com/cprevallet/fitplot/static /opt/nwjs-sdk-v0.35.5-linux-x64/nw . 
